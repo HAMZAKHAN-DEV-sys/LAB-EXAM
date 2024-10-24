78359fe964c7f1e08fa58aa20933f35dc8f200d8
HAMZAKHAN-DEV-sys <hamzakhan84613@gmail.com>
2024-10-24 19:01:13 +0500

    commits
    html and css files are commeted

---
+++ index.js
@@ -0,0 +1,7 @@
+
+const btn = document.querySelector('#hamburguer-btn');
+const btnSeePrjct = document.querySelectorAll('.project-access');
+const closeBtn = document.querySelector('.close-btn');
+const mobilenavlinks = document.querySelectorAll('.m-navlink');
+const sections = document.querySelectorAll('.section');
+
